4ca54000c2d9ba925b8f9af19ab5af3595193b6c
Chenxuyang <foreversimon@hotmail.com>
2020-12-18 17:42:37 +0800

    完成websocket

--- server/express-cli/websocket/index.js
+++ server/express-cli/websocket/index.js
@@ -1,45 +1,25 @@
 const Ws = require('ws')
-const Store = require('./Store')
-const Rx = require('rxjs/Rx')
+const Store = require('@foreversimon/my-store')
+const MongoClient = require('mongodb').MongoClient
+const dbUrl = 'mongodb://localhost:27017/'
 
-let observable = new Rx.Subject()
-observable.subscribe(value => console.log(value))
-observable.next({
-    a: '234'
-})
+const path = '/ws'
 
 // ws全局变量
-
-/**
- * admin: {
-        username: '',
-        ws: null
-    }
- */
-global.wsStore = new Store({})
-/**
- * admin: {
-        ws: null,
-        msgBox: [
-            {username: 'admin', msg: 'init'}
-        ]
-    }
- */
-global.records = new Store({})
+global.wsStore = new Store({
+    allWs: {}
+})
 
 function createWs (server) {
-    const wss = new Ws.Server({server, path: '/ws'})
+    const wss = new Ws.Server({server, path})
     wss.on('connection', function (ws, req, next) {
-        const records = new Store({})
-        const handleChangeWs = onChangeWs(ws, req.cookies)
-        const handleChangeRecords = onChangeRecords(ws, req.cookies)
+        const handleChangeWs = onChangeWs()
         const listenArr = [
-            [wsStore, handleChangeWs, 'change'],
-            [records, handleChangeRecords, 'change']
+            [wsStore, handleChangeWs, 'change']
         ]
         registerListener(listenArr)
         onConnection(ws, req.cookies)
-        ws.on('message', onMessage(ws, req.cookies))
+        ws.on('message', onMessage(req.cookies.username))
         ws.on('error', onError(ws, req.cookies, listenArr))
         ws.on('close', onClose(ws, req.cookies, listenArr))
     })
@@ -47,70 +27,124 @@ function createWs (server) {
 
 function registerListener (arr = []) {
     arr.forEach(item => {
-        item[0].listen(item[2], item[1])
+        item[0].$listen(item[2], item[1])
     })
 }
 
 function removeListener (arr = []) {
     arr.forEach(item => {
-        item[0].listenRemove(item[2], item[1])
+        item[0].$listenRemove(item[2], item[1])
     })
 }
 
-function onChangeWs (ws, cookies) {
-    return function (key, value, oldValue) {
-        if (value === oldValue) return
-        // console.log(key, value, oldValue)
-    }
-}
-
-function onChangeRecords (ws, cookies) {
-    return function (key, value, oldValue) {
-        if (value === oldValue) return
-        // console.log(key, value, oldValue)
-    }
-}
-const dateNow = Date.now() + ''
-function onConnection (ws, cookies) {
-    wsStore.setData(dateNow, {
-        username: 'admin',
-        ws
-    })
-    records.setData(dateNow, {
-        msgBox: [],
-        ws
-    })
-}
-
-function onMessage (ws, cookies) {
-    return function (msg) {
-        const recordsData = records.getData()
-
-        records.setData('', Object.keys(recordsData).reduce((now, value) => {
-            const msgBox = recordsData[value].msgBox ? recordsData[value].msgBox.concat([{
-                msg,
-                username: '',
-                date: Date.now()
-            }]) : []
-            now[value] = {
-                ...recordsData[value],
-                msgBox: msgBox
+function onChangeWs () {
+    return function (value, oldValue, target, key) {
+        if (key === 'allWs') {
+            const objKeys = Object.keys(this.$data.allWs)
+            for (let key of objKeys) {
+                const item = this.$data.allWs[key]
+                const list = objKeys.reduce((base, val) => {
+                    return base.concat([{
+                        username: item.username
+                    }])
+                }, [])
+                item.ws.send(JSON.stringify({
+                    type: 'list',
+                    list
+                }))
             }
-            return now[value]
-        }, {}))
+        }
+    }
+}
+
+function onConnection (ws, cookies) {
+    addWs(ws, cookies)
+}
+
+function onMessage (username) {
+    return function (msg) {
+        try {
+            const msgJSON = JSON.parse(msg)
+            switch (msgJSON.type) {
+                case 'message':
+                    dispatchMsg({username, msg: msgJSON.msg})
+                    break
+                case 'clear':
+                    wsStore.$data.allWs[username].$data.records = []
+                    break
+            }
+        } catch (e) {
+            console.log('非JSON格式')
+        }
     }
 }
 
 function onError (ws, cookies, removeCallback = []) {
     return function () {
+        delWs(cookies.username)
         removeListener(removeCallback)
     }
 }
 
 function onClose (ws, cookies, removeCallback = []) {
     return function () {
+        delWs(cookies.username)
         removeListener(removeCallback)
     }
+}
+
+function dispatchMsg (msg = {username: '', msg: ''}) {
+    const objKeys = Object.keys(wsStore.$data.allWs)
+    for (let key of objKeys) {
+        const item = wsStore.$data.allWs[key]
+        item.$data.records = [...item.records, msg]
+    }
+}
+
+function addWs (ws, {username, password}) {
+    wsStore.$data.allWs = {
+        ...wsStore.$data.allWs,
+        [username]: new Store({
+            username,
+            password,
+            records: [],
+            ws
+        })
+    }
+    registerListener([
+        [wsStore.$data.allWs[username], handleChangeRecords, 'change']
+    ])
+    dispatchMsg({username, msg: '已连接'})
+    MongoClient.connect(dbUrl, function (err, db) {
+        if (err) {
+            console.log(err)
+            return false
+        }
+        const dbo = db.db('users')
+        dbo.collation('list').find({username}).toArray(function (err, result) {
+            if (err) throw err;
+            if (result[0].msgBox) {
+                wsStore.$data.allWs[username].$data.records = result[0].msgBox
+            }
+            db.close();
+        })
+    })
+
+}
+
+function handleChangeRecords (value, oldValue, target, key) {
+    if (key === 'records') {
+        this.$data.ws.send(JSON.stringify({
+            type: 'msg',
+            username: this.$data.username,
+            msgBox: value
+        }))
+    }
+}
+
+function delWs (username) {
+    dispatchMsg({username, msg: '已失去连接'})
+    delete wsStore.$data[username]
 }
 
 module.exports = createWs;
